6796847f6f1aeccab6798853790453e8c886a3c8
Manu Paul <166366565+manu-paul20@users.noreply.github.com>
2026-03-19 16:57:32 +0530

    Delete app/src/main/java/com/manu/novox/others/MyConstants.kt

--- app/src/main/java/com/manu/novox/others/MyConstants.kt
+++
@@ -1,9 +0,0 @@
-package com.manu.novox.others
-
-object MyConstants {
-    object DATABASE{
-        val USERS = "users"
-        val MESSAGES = "messages"
-    }
-    val WEB_CLIENT_ID = "281995683576-rltmdjlpv9od9dq1p8d3earidh3c7tdc.apps.googleusercontent.com"
-}
