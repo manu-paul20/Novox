3f79c28f8043006c5d90346ff137657a080e02bd
Manu Paul <166366565+manu-paul20@users.noreply.github.com>
2026-04-20 21:05:44 +0530

    fixes a bug on image send

--- app/src/main/java/com/manu/novox/presentation/chatscreen/screen/chatScreen.kt
+++ app/src/main/java/com/manu/novox/presentation/chatscreen/screen/chatScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -136,7 +137,10 @@ fun ChatScreen(
                                     model = state.value.imageUrl,
                                     contentScale = ContentScale.Crop,
                                     contentDescription = "selected image",
-                                    modifier = Modifier.clip(RoundedCornerShape(10.dp))
+                                    modifier = Modifier
+                                        .clip(RoundedCornerShape(10.dp))
+                                        .size(100.dp)
+
                                 )
 
                         }
